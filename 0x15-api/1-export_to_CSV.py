728fd6c9165e1fd7bc6b433ec18ba4de705e6dd4
mainbosskay <dadakehinde80@yahoo.com>
2024-05-20 09:53:46 -0700

    Python script that uses REST API, for a given employee ID, returns information about his/her TODO list progress. Export the script to export data in the CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Python script that uses REST API for a given employee ID"""
+from re import fullmatch
+from requests import get
+from sys import argv
+
+
+API_URL = "https://jsonplaceholder.typicode.com"
+"""The RESTAPI's URL"""
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        if fullmatch(r"\d+", argv[1]):
+            empy_id = int(argv[1])
+            empy_resp = get(f"{API_URL}/users/{empy_id}").json()
+            tsk_rep = get(f"{API_URL}/todos").json()
+            empy_name = empy_resp.get("name")
+            tasks = list(filter(lambda k: k.get("userId") == empy_id, tsk_rep))
+            with open(f"{empy_id}.csv", "w") as fl:
+                for tsk in tasks:
+                    fl.write(
+                            f'"{empy_id}","{empy_name}",'
+                            f'"{tsk.get("completed")}","{tsk.get("title")}"\n'
+                    )
