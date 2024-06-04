5ea2e6ea18a788645c64ccffd4af23f5e5759ea9
mainbosskay <dadakehinde80@yahoo.com>
2024-06-04 21:01:40 +0100

    Python program that writes a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Module for interacting with the Reddit API to fetch data"""
 from requests import get
 
@@ -10,18 +10,18 @@ URL_REDDIT = "https://www.reddit.com"
 def number_of_subscribers(subreddit):
     """Retrieves and returns subscribers count for specified subreddit"""
     head_reddit = {
-        "Accept": "applcation/json",
-        "User_Agent": " ".join([
+        "Accept": "application/json",
+        "User-Agent": " ".join([
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 12.7.4)",
             "Firefox/124.0.2",
             "safari/17.4.1"
             ])
     }
-    resquest_data = get(
+    resp_data = get(
             f"{URL_REDDIT}/r/{subreddit}/about.json",
             headers=head_reddit,
             allow_redirects=False
     )
-    if resquest_data.status_code == 200:
-        return resquest_data.json()["data"]["subscribers"]
+    if resp_data.status_code == 200:
+        return resp_data.json()["data"]["subscribers"]
     return 0
