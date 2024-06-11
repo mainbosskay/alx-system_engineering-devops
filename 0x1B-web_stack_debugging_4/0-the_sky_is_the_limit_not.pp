cdc565c9dbfcc8e4cf4862630e2859eb3b6b5288
mainbosskay <dadakehinde80@yahoo.com>
2024-06-11 21:40:54 +0000

    Debugging Nginx web server under pressure

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+#Web debugging Nginx web server under pressure to mitigate failed requests
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
